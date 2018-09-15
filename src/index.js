8faf5c9f3cf54f50deee48082ec3c1149c65bf29
sthuang <sthuang@ucsc.edu>
2018-09-15 13:03:13 -0700

    Added Card Compnent + Rendering and css change

--- src/index.js
+++ src/index.js
@@ -2,12 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Game extends React.Component {
+class Card extends React.Component {
   render() {
+    return (
+      <div className="Card">
+        {this.props.value}
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  constructor(props){
+    super(props);
+    this.renderCards = this.renderCards.bind(this);
+  }
+
+  renderCards(cardVals){
+    return cardVals.map((cardVal, index) => {
+      return (
+        <Card key={index} value={cardVal} />
+      );
+    });
+  }
+
+  render() {
+    var cards = [5, 2, 4, 1, 1, 3, 4, 2, 3, 5];
     return (
       <div className="game">
         <div>Memory Game</div>
         <div className="game-board">
+          {this.renderCards(cards)}
         </div>
       </div>
     );
