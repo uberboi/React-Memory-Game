5ec9ff500b1e5443367c31bd72fe4a41f854fe98
sthuang <sthuang@ucsc.edu>
2018-09-16 01:15:13 -0700

    Card matching logic added

--- src/index.js
+++ src/index.js
@@ -7,9 +7,12 @@ class Card extends React.Component {
   render() {
     var cardValue = this.props.flipped ? this.props.value : '';
     var cardClass = "Card";
-    if(this.props.flipped){
+    if(this.props.matched){
+      cardClass += " Card--matched";
+    }else if(this.props.flipped){
         cardClass += " Card--flipped";
     }
+
     return (
       <div
         className = {cardClass}
@@ -38,15 +41,38 @@ class Game extends React.Component {
         {value : 2, matched : false, flipped : false},
         {value : 3, matched : false, flipped : false},
         {value : 5, matched : false, flipped : false},
-      ]
+      ],
+      firstCard: null
     }
   }
 
   handleClick(i){
     //this.state.cards[i] = this.setState({flipped: !this.state.flipped});
+    //var cards = this.state.cards;
+    //cards[i].flipped = !cards[i].flipped;
+    //this.setState({cards});
     var cards = this.state.cards;
-    cards[i].flipped = !cards[i].flipped;
+    const value = cards[i].value;
+    cards[i].flipped = true;
     this.setState({cards});
+    if(this.state.firstCard){
+      if(value === cards[this.state.firstCard].value){
+        cards[i].matched = true;
+        cards[this.state.firstCard].matched = true
+        this.setState({cards, firstCard: null});
+      }else{
+        setTimeout(() => {
+          cards[i].flipped = false;
+          cards[this.state.firstCard].flipped = false;
+          this.setState({cards, firstCard: null});
+        }, 1000);
+      }
+    }else{
+      this.setState({
+        firstCard: i
+      });
+    }
+
   }
 
   renderCards(cards){
@@ -62,6 +88,29 @@ class Game extends React.Component {
         />
       );
     });
+  }
+
+  checkMatch(id){
+    var cards = this.state.cards;
+    const value = cards[id].value;
+    cards[id].flipped = true;
+    this.setState({cards});
+    if(this.state.firstCard){
+      if(value === this.state.firstCard.value){
+        cards[id].matched = true;
+        cards[this.state.firstCard.id].matched = true;
+      }else{
+        setTimeout(() => {
+          cards[id.flipped] = false;
+          cards[this.state.firstCard.id].flipped = false;
+          this.setState({cards, firstCard: null});
+        }, 1000);
+      }
+    }else{
+      this.setState({
+        firstCard: id
+      });
+    }
   }
 
   render() {
